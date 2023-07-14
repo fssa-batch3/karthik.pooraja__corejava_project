7f44eaf6e82498380be4846c4b280e64c77d6d8d
KarthikRaja0517 <116252119+KarthikRaja0517@users.noreply.github.com>
2023-07-14 16:33:15 +0530

    day 07 solved ones

--- src/main/java/day04/practice/PolygonTest.java
+++ src/main/java/day04/practice/PolygonTest.java
@@ -4,14 +4,15 @@ package day04.practice;
 class PolygonTest {
 	public static void main(String[] args) {
 		Polygon rec;
+		Polygon rat;
 		try {
 			
 			rec = new Rectangle (10, 5);
 			rec.calculateArea();
 			rec.circumference();
-			RightAngledTriangle ratt = new RightAngledTriangle(4.0, 3.0,5.0);
-			ratt.calculateArea();
-			ratt.circumference();		
+			rat = new RightAngledTriangle(4.0, 3.0,5.0);
+			rat.calculateArea();
+			rat.circumference();		
 			
 		} catch (Exception e) {
 			e.printStackTrace();
